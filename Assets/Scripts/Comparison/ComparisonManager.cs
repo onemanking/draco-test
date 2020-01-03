2f54698b5fb91ca3ebdca6189d38097f7233f405
Rachen.Nueng <prd.rachen@gmail.com>
2020-01-03 15:08:13 +0700

    Added comparison scene

---
+++ Assets/Scripts/Comparison/ComparisonManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+
+public class ComparisonManager : MonoBehaviour
+{
+	[SerializeField] private MeshFilter m_Obj;
+	[SerializeField] private MeshFilter m_Drc;
+
+	private void Start()
+	{
+		Stopwatch watch = new Stopwatch();
+		var dracoLoader = new DracoMeshLoader();
+		var mesh = new List<Mesh>();
+
+		watch.Start();
+		m_Obj.mesh = Resources.Load("MachineGun", typeof(Mesh)) as Mesh;
+		watch.Stop();
+		Debug.LogWarning($"FINISH TIME OBJ { watch.Elapsed.TotalSeconds }");
+
+		watch.Reset();
+
+		watch.Start();
+		dracoLoader.LoadMeshFromAsset("Models/gun/MachineGun.drc", ref mesh);
+		m_Drc.mesh = mesh[0];
+		watch.Stop();
+		Debug.LogWarning($"FINISH TIME DRC { watch.Elapsed.TotalSeconds }");
+	}
+}
---
+++ Assets/Scripts/Comparison/RotateObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+	[SerializeField] private float m_Speed = 10;
+	[SerializeField] private float m_X;
+	[SerializeField] private float m_Y = 1;
+	[SerializeField] private float m_Z;
+
+	void Start()
+	{
+		Observable.EveryUpdate().Subscribe(_ =>
+			{
+				transform.Rotate(m_Speed * Time.deltaTime * new Vector3(m_X, m_Y, m_Z));
+
+			}).AddTo(this);
+	}
+}
