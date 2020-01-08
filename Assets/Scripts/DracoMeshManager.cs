d83802e154e04f1d3fd8eea887209492832c4b42
Rachen.Nueng <prd.rachen@gmail.com>
2020-01-08 13:29:34 +0700

    [Added] added liquid model and adjust draco script

--- Assets/Scripts/DracoMeshManager.cs
+++ Assets/Scripts/DracoMeshManager.cs
@@ -5,132 +5,65 @@ using System.Linq;
 using UniRx;
 using Unity.Collections;
 using UnityEngine;
-using UnityEngine.Profiling;
 
 public class DracoMeshManager : MonoBehaviour
 {
 	private const string _MODEL_FOLDER = "Models";
-
 	public static DracoMeshManager Instance => _Instance;
 
-	public bool IsInitialized => _IsInitialized;
-
 	private static DracoMeshManager _Instance;
-
-	private Dictionary<string, List<Mesh>> _MeshDict = new Dictionary<string, List<Mesh>>();
-	private bool _IsInitialized;
-
-#if USE_RUNTIME_LOADER
-	private Dictionary<string, List<string>> _ModelDict;
-	private DracoMeshLoader _DracoLoader;
-#endif
 
 	void Awake()
 	{
 		_Instance = GetComponent<DracoMeshManager>();
-		InitResources();
+		DontDestroyOnLoad(this);
 	}
 
-	private void InitResources()
+	private DracoMeshLoader _DracoLoader = new DracoMeshLoader();
+	public IObservable<Mesh[]> GetMeshListAsObservable(string _modelName)
 	{
-		var root = new DirectoryInfo(Path.Combine(Application.streamingAssetsPath, _MODEL_FOLDER));
-
-#if !USE_RUNTIME_LOADER
-		var modelDict = new Dictionary<string, List<string>>();
-		var dracoLoader = new DracoMeshLoader();
-		int count = 0;
-
-		foreach (var dir in root.GetDirectories())
+		return Observable.Create<Mesh[]>(_observer =>
 		{
-			var files = dir.GetFiles("*.drc.bytes", SearchOption.AllDirectories);
-			modelDict.Add(dir.Name, files.Select(_ => _.Name).OrderBy(x => x).ToList());
-		}
+			var allFiles = Directory.GetFiles(Path.Combine(Application.streamingAssetsPath, _MODEL_FOLDER, _modelName), "*.drc.bytes", SearchOption.AllDirectories).OrderBy(x => x).ToArray();
+			var meshs = new Mesh[allFiles.Length];
+			var disposable = new CompositeDisposable();
 
-		foreach (var key in modelDict.Keys)
-		{
-			var meshList = new List<Mesh>();
-			foreach (var asset in modelDict[key])
+			for (int i = 0; i < allFiles.Length; i++)
 			{
-#if UNITY_IOS
-				var url = "file://" + Path.Combine(Application.streamingAssetsPath, _MODEL_FOLDER, key, asset);
-#elif UNITY_EDITOR
-				var url = Path.Combine(Application.streamingAssetsPath, _MODEL_FOLDER, key, asset);
-#endif
-				ObservableWWW.GetAndGetBytes(url)
-				.Subscribe
+				string file = allFiles[i];
+				var index = i;
+				var path = "file://" + file;
+				disposable.Add
 				(
-					_byte =>
-					{
-						Debug.LogWarning($"URL : {url} , BYTES : {_byte.Length}");
-						var data = new NativeArray<byte>(_byte, Allocator.Persistent);
-						Observable.FromCoroutine<Mesh>((_observer) => dracoLoader.DecodeMesh(data, _observer))
-								.Subscribe(_mesh =>
-								{
-									meshList.Add(_mesh);
-									count += 1;
-									_IsInitialized = count >= modelDict.Count();
-								})
-								.AddTo(this);
-					}, _error =>
-					{
-						Debug.LogError(_error);
-					}
-				).AddTo(this);
-			}
-			_MeshDict.Add(key, meshList);
-		}
-#else
-		_DracoLoader = new DracoMeshLoader();
-		_ModelDict = new Dictionary<string, List<string>>();
-		foreach (var dir in root.GetDirectories())
-		{
-			var files = dir.GetFiles("*.drc.bytes", SearchOption.AllDirectories);
-			_ModelDict.Add(dir.Name, files.Select(_ => _.Name).OrderBy(x => x).ToList());
-		}
-		_IsInitialized = true;
-#endif
-	}
-
-#if !USE_RUNTIME_LOADER
-	public int ListCount(string _modelName) => _MeshDict[_modelName].Count;
-
-	public Mesh GetMesh(string _modelName, int _index) => _MeshDict[_modelName][_index];
-#else
-	public int ListCount(string _modelName) => _ModelDict[_modelName].Count;
-
-	public IObservable<Mesh> GetMesh(string _modelName, int _index)
-	{
-
-#if UNITY_IOS
-		var url = "file://" + Path.Combine(Application.streamingAssetsPath, _MODEL_FOLDER, _modelName, _ModelDict[_modelName][_index]);
-#elif UNITY_EDITOR
-		var url =  Path.Combine(Application.streamingAssetsPath, _MODEL_FOLDER, _modelName, modelDict[_modelName][_index]);
-#endif
-		return Observable.Create<Mesh>(_observer =>
-		{
-			var disposable = ObservableWWW.GetAndGetBytes(url)
-								.Subscribe
-								(
-									_byte =>
-									{
-										Debug.LogWarning($"URL : {url} , BYTES : {_byte.Length}");
-										var data = new NativeArray<byte>(_byte, Allocator.Persistent);
-										Observable.FromCoroutine<Mesh>((_) => _DracoLoader.DecodeMesh(data, _))
-												.Subscribe(_mesh =>
+					ObservableWWW.GetAndGetBytes(path)
+						.Subscribe
+						(
+							_byte =>
+							{
+								var data = new NativeArray<byte>(_byte, Allocator.Persistent);
+								Observable.FromCoroutine<Mesh>((_) => _DracoLoader.DecodeMesh(data, _))
+										.Subscribe
+										(
+											_mesh =>
+											{
+												meshs[index] = _mesh;
+												_observer.OnNext(meshs);
+												if (index >= allFiles.Length - 1)
 												{
-													_observer.OnNext(_mesh);
 													_observer.OnCompleted();
-												}, () => data.Dispose());
-									}, _error =>
-									{
-										Debug.LogError(_error);
-									}
-								);
+												}
+											}
+										);
+							},
+							_error =>
+							{
+								_observer.OnError(_error);
+							}
+						)
+				);
+			}
 
 			return Disposable.Create(() => disposable?.Dispose());
 		});
 	}
-
-#endif
-
 }
